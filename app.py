2558bf928882ffb21ffbf4b3edcf7322f19bdcd1
malgeum <kwonsunny@naver.com>
2025-11-02 23:32:46 +0900

    재수정

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_mistralai.embeddings import MistralAIEmbeddings
 from langchain_chroma import Chroma
