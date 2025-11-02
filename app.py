b80d7418c78235f262756a6a61717fa97c25489a
malgeum <kwonsunny@naver.com>
2025-11-02 23:34:48 +0900

    재재ㅜ섲ㅇ

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_mistralai.embeddings import MistralAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough , RunnableWithMessageHistory
 from langchain.memory import ChatMessageHistory
 
